09ec65822b615e34b745acf50249a367da9731eb
JPFrancoia <jeanpatrick.francoia@gmail.com>
2025-08-28 17:14:17 +0100

    Fixing SQL query

--- feedoscope/data_registry/sql/get_previous_days_wo_time_sensitivity_articles.sql
+++ feedoscope/data_registry/sql/get_previous_days_wo_time_sensitivity_articles.sql
@@ -9,10 +9,11 @@ select
     e.author,
     e.date_entered,
     ue.last_read,
-    array_agg(distinct l.caption) filter (where l.caption is not null), array[]::text[] as labels,
-    array_agg(distinct t.tag_name) filter (where t.tag_name is not null), array[]::text[] as tags
+    coalesce(array_agg(distinct l.caption) filter (where l.caption is not null), array[]::text[]) as labels,
+    coalesce(array_agg(distinct t.tag_name) filter (where t.tag_name is not null), array[]::text[]) as tags
 from
-    ttrss_user_entries ue on e.id = ue.ref_id
+    ttrss_entries e
+    join ttrss_user_entries ue on e.id = ue.ref_id
     join ttrss_feeds f on ue.feed_id = f.id
     left join ttrss_user_labels2 ul on e.id = ul.article_id
     left join ttrss_labels2 l on ul.label_id = l.id
@@ -29,6 +30,7 @@ group by
     ue.marked,
     ue.score,
     f.title,
+    e.content,
     e.link,
     e.author,
     e.date_entered,
